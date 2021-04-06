6108f63b8030c1fd10c9de176c10140206181313
unknown <pigatapsoba@gmail.com>
2021-04-06 17:00:22 +0000

    creation de la page d'accueil

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,66 @@
+@extends('layout.template')
+
+@section('content')
+<div style="margin-top: 10%;margin-left:-40%; ">
+	<div class="row">
+    <div class="col-md-3 col-sm-6 col-12">
+      <a href="#">
+      <div class="info-box">
+        <span class="info-box-icon bg-info">
+         <img src="{{asset('img/icon_formation.png')}}" alt="">
+        </span>
+
+        <div class="info-box-content">
+          <span class="info-box-text formation_panel" >Gestion de la formation</span>
+          <span class="info-box-number">
+            
+          </span>
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+    </a>
+      <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-12">
+      <a href="#">
+      <div class="info-box">
+        <span class="info-box-icon bg-success">
+          <img src="{{asset('img/icon_foncier.png')}}" alt="">
+        </span>
+
+        <div class="info-box-content">
+          <span class="info-box-text foncier_panel" style="color:#28a745">Gestion du foncier rural</span>
+          <span class="info-box-number">
+
+          </span>
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+    </a>
+      <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-warning">
+          <img src="{{asset('img/icon_foncier.png')}}" alt="">
+        </span>
+
+        <div class="info-box-content">
+          <span class="info-box-text">Gestion organisation paysante</span>
+          <span class="info-box-number">
+
+          </span>
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+      <!-- /.info-box -->
+    </div>
+   
+  </div>
+ 
+    
+</div>
+
+@endsection
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,145 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
+  <title>DGFOMR</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('plugins/toastr/toastr.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dist/js/example-styles.css')}}">
+  <link rel="stylesheet" href="{{asset('dist/css/print.min.css')}}">
+  <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
+  <link rel="stylesheet" href="{{asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('styles/accueil_style.css')}}">
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class=" navbar navbar-expand navbar-white navbar-light" style="background-color: #926d27">
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+       <!-- Right navbar links -->
+       <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="fa fa-user-circle fa-2x" style="color:white"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          
+            <div class="dropdown-divider"></div>
+           
+            <a  class="nav-link" href="{{ url('users') }}"><i style="color:#007bff;" class="fa fa-users"></i> Utilisateurs</a>
+         
+            <div class="dropdown-divider"></div>
+            
+          </div>
+        </li>
+      </ul>
+
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+
+  
+<div class="row">
+  <div class="col-lg-4 col-md-4 col-sm-4">
+
+  </div>
+   <div class="col-lg-8 col-md-8 col-sm-8">
+   <div class="row">
+     <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
+   <section class="content">
+    <div class="container-fluid">
+      @yield('content')
+    </div>
+   </section>
+     </div>
+   </div>
+  </div>
+</div>
+
+<div class="row" style="margin-top: 20%;">
+  <div class="col-lg-12 col-md-12 col-sm-12 ">
+     <!-- Main Footer -->
+  <footer  style="background-color: #926d27; color: white;height: 100px;" align='center'>
+    <strong >Copyright &copy; 2019-2020 <a href="http://adminlte.io">BUMIGEB</a>.</strong>
+    All rights reserved.
+  
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset('dist/js/jquery-3.5.1.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dist/js/adminlte.js')}}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{asset('dist/js/demo.js')}}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{asset('plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+<script src="{{asset('plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
+
+<!-- PAGE SCRIPTS -->
+<script src="{{asset('dist/js/pages/dashboard2.js')}}"></script>
+
+<script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('dist/js/scriptdatable.js')}}"></script>
+<script src="{{asset('plugins/toastr/toastr.min.js')}}"></script>
+<script src="{{asset('dist/js/verifier.js')}}"></script>
+<script src="{{asset('dist/js/jquery.multi-select.js')}}"></script>
+<script src="{{asset('dist/js/jquery.multi-select.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-validation/additional-methods.min.js')}}"></script>
+<script src="{{asset('dist/js/print.min.js')}}"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Select2 -->
+<script src="{{asset('plugins/select2/js/select2.full.min.js')}}"></script>
+<script src="{{asset('dist/js/saveComposition.js')}}"></script>
+
+</body>
+</html>
+
+  </div>
+</div>
